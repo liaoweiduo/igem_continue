b994553bb23bae67a494029f707759f9bce0334a
teddy <liaowd@sustc.edu.cn>
2017-03-18 23:39:16 +0800

    divbackground

---
+++ DivBackground.m
@@ -0,0 +1,8 @@
+function [ OutImg ] = DivBackground( InImg , BgX , BgY, BgLength , BgWidth )
+    BgImg  = InImg(BgY:BgY+BgWidth,BgX:BgX+BgLength);
+    BgImg_t = BgImg(:);
+    BgImg_t = sort(BgImg_t,'descend');
+    BgInt = mean(BgImg_t((BgLength*BgWidth*0.01) : (BgLength*BgWidth*0.11)));
+
+    OutImg = InImg-BgInt;
+end
