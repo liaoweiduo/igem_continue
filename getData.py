37aaa90f89f677a1c980576cee8ec788a225e0f8
teddy <liaowd@sustc.edu.cn>
2017-04-12 12:18:06 +0800

    unfinished

--- getData.py
+++ getData.py
@@ -3,19 +3,60 @@ from pylab import *
 from PIL import Image
 import copy
 
+class Cell:
+    cellsCount = 0
+    def __init__(self, topX, topY):
+        self.topX=topX
+        self.topY=topY
+        self.centralX=topX
+        self.centralY=topY
+        self.downX=topX
+        self.downY=topY
+        self.leftX=topX
+        self.leftY=topY
+        self.rightX=topX
+        self.rightY=topY
+        Cell.cellsCount+=1
+
 generalPath="/Volumes/Seagate BUP/IGEM_new/20170318/5ul/piezo+5ult%03dc1.tif"
 im = np.array(Image.open(generalPath % (1),"r"))
 
-#get threadHold  = average(max,min)
+sign = zeros(im.shape[0], im.shape[1])
+# get threadHold  = average(max,min)
+
+lenX=im.shape[0]
+lenY=im.shape[1]
+lenT=lenX*lenY
 
 sortedIm=copy.copy(im)
 sortedIm.sort()
 
-min=average(sortedIm[0:100])
-max=average(sortedIm[])
+print('totalLen: ',lenT)
 
-print(sortedIm[100000:])
-threadHold=average(im)
-print(threadHold)
+min=average(sortedIm[0:lenT*0.1])
+max=average(sortedIm[lenT*0.9:lenT])
+threadHold=(min+max)/2
+
+print('min: ',min,' max: ',max,' threadHold: ',threadHold)
+
+# sign     0:unVisit  1:cell1 ...   -1:background
+
+def signExpend(x, y):
+    Flag = Cell.cellsCount
+    if sign[x,y] == -1:
+        return
+    elif sign[x,y] >0 and sign[x,y]!=Flag:    #与其他细胞相撞
+
+    if im[i,j]<threadHold:
+        sign[i,j]=-1
+    else:
 
 
+for i in range(0,lenX):
+    for j in range(0,lenY):
+        if sign[i,j]==0:
+            if im[i,j]<threadHold:
+                sign[i,j]=-1
+            else:
+                newCell=Cell(i,j)
+                signExpend(i,j)
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import numpy as np
 from pylab import *
 from PIL import Image
-im = array(Image.open("20170318_1ul.jpg","r"))
+im = array(Image.open("data/20170318_1ul.jpg","r"))
 
 
 #输出数组的各维度长度以及类型
@@ -13,6 +13,8 @@ print (im[100,100,0])
 #输出坐标100,100的rgb值
 print (im[100,100])
 
+print(im.shape[1])
+
 #im.show()
 
 # 一些点
@@ -20,12 +22,16 @@ x = [100,100,400,400]
 y = [200,500,200,500]
 
 # 使用红色星状标记绘制点
-plot(x,y,'r*')
+
+#plot(x,y,'r*')
 
 # 绘制连接前两个点的线
-plot(x[:2],y[:2])
+
+#plot(x[:2],y[:2])
 
 # 添加标题,显示绘制的图像
-title('Plotting: "Test.jpg"')
 
-show()
+#title('Plotting: "Test.jpg"')
+
+#show()
+
