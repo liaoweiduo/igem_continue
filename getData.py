c17cb91dc8ca1c92dbf5e17c977d79610f32ccb0
teddy <liaowd@sustc.edu.cn>
2017-04-13 17:21:37 +0800

    temp

--- getData.py
+++ getData.py
@@ -1,62 +1,175 @@
 import numpy as np
+from MyGaussianBlur import MyGaussianBlur
 from pylab import *
 from PIL import Image
 import copy
+import queue
+import matplotlib.pyplot as plt
+
+generalPath = "/Volumes/Seagate BUP/IGEM_new/20170318/5ul/piezo+5ult%03dc1.tif"
+#generalPath = "piezot%03dc1.tif"
+
+photosSize = 500 # the num of photos
+
+minCellSize = 50  # a cell is bigger than $ pixels
+maxCellSize = 1024  # a cell is smaller than $ pixels
+ThreadHoldRate = 0.15  # Cell threadhold rate
+
+r = 2  # 模版半径，自己自由调整
+s = 2  # sigema数值，自己自由调整
+
 
 class Cell:
     cellsCount = 0
+
     def __init__(self, topX, topY):
-        self.topX=topX
-        self.topY=topY
-        self.centralX=topX
-        self.centralY=topY
-        self.downX=topX
-        self.downY=topY
-        self.leftX=topX
-        self.leftY=topY
-        self.rightX=topX
-        self.rightY=topY
-        Cell.cellsCount+=1
+        self.topPoint = (topX, topY)
+        self.downPoint = (topX, topY)
+        self.leftPoint = (topX, topY)
+        self.rightPoint = (topX, topY)
+        self.cellSize = 0  # count pixel of cell
+        Cell.cellsCount += 1
+        self.cellNo = Cell.cellsCount
+        self.fluorescenceTable = []
+        self.maxFluo = 0
+        self.maxFluoIndex = 0
 
-generalPath="/Volumes/Seagate BUP/IGEM_new/20170318/5ul/piezo+5ult%03dc1.tif"
-im = np.array(Image.open(generalPath % (1),"r"))
+img = Image.open(generalPath % (1), 'r')
+im = np.array(img)
 
-sign = zeros(im.shape[0], im.shape[1])
+# print('Im: \n', im)
+
+lenX = int(im.shape[0])
+lenY = int(im.shape[1])
+lenT = lenX * lenY
+
+plt.subplot(1,2,1)
+plt.imshow(im)  # 显示图片
+plt.title("Choose Background")
+
+# GaussianBlur
+GBlur = MyGaussianBlur(radius = r, sigema = s)  # 声明高斯模糊类
+temp = GBlur.template()  # 得到滤波模版
+image = GBlur.filter(img, temp)  # 高斯模糊滤波，得到新的图片
+im=np.array(image)
+
+bgPoint = ginput(2)
+bgValue = int(im[int(bgPoint[0][1]) : int(bgPoint[1][1]), int(bgPoint[0][0]) : int(bgPoint[1][0])].mean())
+
+for i in range(0, lenX):
+    for j in range(0, lenY):
+        if im[i][j] < bgValue:
+            im[i][j] = 0
+        else:
+            im[i][j] -= bgValue
+
+plt.imshow(im)
+
+label = np.zeros([im.shape[0], im.shape[1]], int16)
+
 # get threadHold  = average(max,min)
 
-lenX=im.shape[0]
-lenY=im.shape[1]
-lenT=lenX*lenY
-
-sortedIm=copy.copy(im)
+sortedIm = copy.copy(im)
+sortedIm.shape = [1, -1]
+sortedIm = sortedIm[0]
 sortedIm.sort()
 
-print('totalLen: ',lenT)
+# print('totalLen: ', lenT)
+# print('Im(-): \n', im)
+# print('sortedIm: \n', sortedIm)
+# print(sortedIm.shape)
+minAve = sortedIm[0]
+maxAve = sortedIm[int(lenT)-1]
+threadHold = minAve * (1 - ThreadHoldRate) + maxAve * ThreadHoldRate
+# print('min: ', minAve, ' max: ', maxAve, ' threadHold: ', threadHold)
 
-min=average(sortedIm[0:lenT*0.1])
-max=average(sortedIm[lenT*0.9:lenT])
-threadHold=(min+max)/2
+# label     0:unVisit  1:cellNo ...   -1:background
+def labelExpend(cell, i, j):
+    flag = cell.cellNo
+    fifo = queue.Queue()
+    fifo.put((i, j))
 
-print('min: ',min,' max: ',max,' threadHold: ',threadHold)
-
-# sign     0:unVisit  1:cell1 ...   -1:background
-
-def signExpend(x, y):
-    Flag = Cell.cellsCount
-    if sign[x,y] == -1:
-        return
-    elif sign[x,y] >0 and sign[x,y]!=Flag:    #与其他细胞相撞
-
-    if im[i,j]<threadHold:
-        sign[i,j]=-1
-    else:
-
-
-for i in range(0,lenX):
-    for j in range(0,lenY):
-        if sign[i,j]==0:
-            if im[i,j]<threadHold:
-                sign[i,j]=-1
+    while fifo.qsize() != 0:
+        point = fifo.get()
+        if label[point[0], point[1]] == 0:
+            if im[point[0], point[1]] > threadHold:
+                label[point[0], point[1]] = flag
             else:
-                newCell=Cell(i,j)
-                signExpend(i,j)
+                label[point[0], point[1]] = -1
+                continue
+            cell.cellSize += 1
+            if label[point[0], point[1] - 1] >= 0:  # left
+                fifo.put((point[0], point[1] - 1))
+            if label[point[0], point[1] + 1] >= 0:  # right
+                fifo.put((point[0], point[1] + 1))
+            if label[point[0] + 1, point[1]] >= 0:  # down
+                fifo.put((point[0] + 1, point[1]))
+            # 更新此point到cell边界中
+            if cell.leftPoint[1] > point[1]:
+                cell.leftPoint = point
+            if cell.rightPoint[1] < point[1]:
+                cell.rightPoint = point
+            if cell.downPoint[0] < point[0]:
+                cell.downPoint = point
+        elif label[point[0], point[1]] != -1 and label[point[0], point[1]] != flag:     # 和已存在细胞相撞 判断是否删掉2个细胞
+            cell2No = label[point[0], point[1]]
+            # print('join occurred, the cell is: ',cell2No)
+            if cell.cellSize > minCellSize/2:
+                removeCellByNo(cellList, cell2No)
+                # print('2 cell join, remove: ', cell2No, ' for the reason that cellSize = ', cell.cellSize)
+                cell.cellSize = 0                      # 跳出程序后删
+            return
+
+def removeCellByNo(cellList, cellNo):
+    for cell in cellList:
+        if cell.cellNo == cellNo:
+            cellList.remove(cell)
+            return True
+    return False
+
+def fluoExpend(cell, im):
+    result = 0
+    centerPoint = (int((cell.downPoint[0] + cell.topPoint[0]) / 2), int((cell.rightPoint[1] - cell.leftPoint[1]) / 2))
+    fifo = queue.Queue()
+    fifo.put(centerPoint)
+    while fifo.qsize() != 0:
+        point = fifo.get()
+        if label[point[0], point[1]] == cell.cellNo:
+            result += im[point[0], point[1]]
+
+cellList = []
+
+plt.subplot(1,2,2)
+
+for i in range(0, lenY):
+    for j in range(0, lenX):
+        if label[i, j] == 0:
+            if im[i, j] < threadHold:
+                label[i, j] = - 1
+            else:
+                cell = Cell(i, j)
+                # print('start expend cell: ', cell.cellNo, ' start at position: ', i, '行, ', j, '列')
+                cellList.append(cell)
+                labelExpend(cell, i, j)  # 函数内给label（i，j）赋值
+                # print('finally get pixel: ', cell.cellSize)
+                if cell.cellSize < minCellSize or cell.cellSize > maxCellSize:
+                    # print('remove from cellList')
+                    cellList.remove(cell)
+
+'''待加入check cellList，删除圆形细胞及非细胞的代码'''
+print('finish init')
+plt.imshow(label)
+
+for i in range(1, photosSize + 1):
+    img = Image.open(generalPath % (i), 'r')
+    im = np.array(img)
+
+    for i in range(0, lenX):
+        for j in range(0, lenY):
+            if im[i][j] < bgValue:
+                im[i][j] = 0
+            else:
+                im[i][j] -= bgValue
+
+    for cell in cellList:
+
--- test.py
+++ test.py
@@ -1,37 +1,109 @@
 import numpy as np
 from pylab import *
 from PIL import Image
-im = array(Image.open("data/20170318_1ul.jpg","r"))
+import matplotlib.pyplot as plt
+from MyGaussianBlur import MyGaussianBlur
+import queue
+
+# generalPath = "/Volumes/Seagate BUP/IGEM_new/20170318/5ul/piezo+5ult001c1.tif"
+img = Image.open("piezot001c1.tif")
+im = np.array(img)
+plt.subplot(1,2,1)
+plt.imshow(im)
+
+r = 4  # 模版半径，自己自由调整
+s = 2  # sigema数值，自己自由调整
+#GBlur = MyGaussianBlur(radius=r, sigema=s)  # 声明高斯模糊类
+#temp = GBlur.template()  # 得到滤波模版
+#image = GBlur.filter(img, temp)  # 高斯模糊滤波，得到新的图片
+#plt.imshow(np.array(image))  # 图片显示
+
+#print(im.shape, im.dtype)
+
+# 输出坐标100,100的rgb值
+print(im[1, 2])
+print(im[1][2])
+print(im[:,:])
 
 
-#输出数组的各维度长度以及类型
-print (im.shape,im.dtype)
+print(len(im))
 
-#输出位于坐标100,100，颜色通道为r的像素值
-print (im[100,100,0])
+print(im)
 
-#输出坐标100,100的rgb值
-print (im[100,100])
+# multiply each pixel by 1.2
 
-print(im.shape[1])
-
-#im.show()
+# im.show()
 
 # 一些点
-x = [100,100,400,400]
-y = [200,500,200,500]
+x = [100, 100, 400, 400]
+y = [200, 500, 200, 500]
 
 # 使用红色星状标记绘制点
 
-#plot(x,y,'r*')
+# plot(x,y,'r*')
 
 # 绘制连接前两个点的线
 
-#plot(x[:2],y[:2])
+# plot(x[:2],y[:2])
 
 # 添加标题,显示绘制的图像
 
-#title('Plotting: "Test.jpg"')
+# title('Plotting: "Test.jpg"')
 
-#show()
+# show()
 
+
+a = 1
+b = a
+
+b += 1
+
+print(a)
+print(b)
+
+
+class aa:
+    def __init__(self, x):
+        self.x = x
+
+
+a = aa(2)
+b = a
+
+b.x = 5
+
+print(a.x)
+print(b.x)
+
+fifo = queue.Queue()
+print(fifo.qsize())
+fifo.put((1, 2))
+print(fifo.qsize())
+fifo.put((3, 2))
+print(fifo.qsize())
+
+print(fifo.get()[1])
+
+print(fifo.qsize())
+
+a = zeros([5, 5], int8)
+
+print(a)
+
+plt.imshow(im)  # 显示图片
+# plt.axis('off')
+# x=ginput(3)
+
+# print(x[1][1])
+
+print(im[0:2,0:3])
+print(im[0:2,0:3].mean())
+
+
+po=(2,1)
+a[po]=3
+a[1,2]=4
+a[1][2]=5
+print (a)
+
+print (po+(2,1))
